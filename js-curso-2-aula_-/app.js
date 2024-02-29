b1eb914a2dabd1e6fe5c3e1d4a2517965486234c
nnibo <nbaradel4@gmail.com>
2024-02-28 22:46:25 -0300

    jogo numeroSecreto

--- js-curso-2-aula_-/app.js
+++ js-curso-2-aula_-/app.js
@@ -1,4 +1,5 @@
-let numeroSecreto = gerarNumeroAleatorio();
+let numeroSecreto = 5
+let tentativas = 1
 
 // let titulo = document.querySelector('h1');
 // titulo.innerHTML = 'Jogo do numero secreto'
@@ -15,10 +16,24 @@ exibirTextoTela('p','Escolha um numero entre 1 a 10')
 
 function verificarChute(){
     let chute = document.querySelector('input').value
-    console.log(chute == numeroSecreto)
+    if(chute == numeroSecreto){
+        exibirTextoTela('h1','Voce acertou o numero!')
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
+        let mensagemTentativa = 'Voce descobriu o numero secreto com' + tentativas + palavraTentativa
+        exibirTextoTela('p',mensagemTentativa)
+    } else {
+        if(chute > numeroSecreto){
+            exibirTextoTela('p','O numero secreto é menor q o chute')   
+        } else {
+            exibirTextoTela('p','O numero secreto é maior q o chute')
+        }
+        tentativas += 1
+        
+    }
 }
 
 function gerarNumeroAleatorio(){
     return parseInt(Math.random() * 10 + 1)
 }
+
 
